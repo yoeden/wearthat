8c2caa2eaec171bf1a948dbc9a95b5e6ef0eafda
yoeden <yoeden@changes.org>
2024-02-11 17:13:04 +0200

    Added wear android deps & set minimum sdk to 26

--- test/wear_method_channel_test.dart
+++ test/wear_method_channel_test.dart
@@ -5,23 +5,24 @@ import 'package:wear/wear_method_channel.dart';
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  MethodChannelWear platform = MethodChannelWear();
-  const MethodChannel channel = MethodChannel('wear');
+  // MethodChannelWear platform = MethodChannelWear();
+  // const MethodChannel channel = MethodChannel('wear');
 
-  setUp(() {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(
-      channel,
-      (MethodCall methodCall) async {
-        return '42';
-      },
-    );
-  });
+  // setUp(() {
+  //   TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(
+  //     channel,
+  //     (MethodCall methodCall) async {
+  //       return '42';
+  //     },
+  //   );
+  // });
 
-  tearDown(() {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(channel, null);
-  });
+  // tearDown(() {
+  //   TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(channel, null);
+  // });
 
-  test('getPlatformVersion', () async {
-    expect(await platform.getPlatformVersion(), '42');
-  });
+  // test('getPlatformVersion', () async {
+  //   expect(await platform.getPlatformVersion(), '42');
+  // });
+  test("STUB", () => expect(true, false));
 }
--- test/wear_test.dart
+++ test/wear_test.dart
@@ -4,26 +4,28 @@ import 'package:wear/wear_platform_interface.dart';
 import 'package:wear/wear_method_channel.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
-class MockWearPlatform
-    with MockPlatformInterfaceMixin
-    implements WearPlatform {
+// class MockWearPlatform
+//     with MockPlatformInterfaceMixin
+//     implements WearPlatform {
 
-  @override
-  Future<String?> getPlatformVersion() => Future.value('42');
-}
+//   @override
+//   Future<String?> getPlatformVersion() => Future.value('42');
+// }
 
 void main() {
-  final WearPlatform initialPlatform = WearPlatform.instance;
+  // final WearPlatform initialPlatform = WearPlatform.instance;
 
-  test('$MethodChannelWear is the default instance', () {
-    expect(initialPlatform, isInstanceOf<MethodChannelWear>());
-  });
+  // test('$MethodChannelWear is the default instance', () {
+  //   expect(initialPlatform, isInstanceOf<MethodChannelWear>());
+  // });
 
-  test('getPlatformVersion', () async {
-    Wear wearPlugin = Wear();
-    MockWearPlatform fakePlatform = MockWearPlatform();
-    WearPlatform.instance = fakePlatform;
+  // test('getPlatformVersion', () async {
+  //   Wear wearPlugin = Wear();
+  //   MockWearPlatform fakePlatform = MockWearPlatform();
+  //   WearPlatform.instance = fakePlatform;
 
-    expect(await wearPlugin.getPlatformVersion(), '42');
-  });
+  //   expect(await wearPlugin.getPlatformVersion(), '42');
+  // });
+
+  test("STUB", () => expect(true, false));
 }
